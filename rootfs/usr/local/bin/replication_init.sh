79bd409bc90e1e7279beee06d9956c5481f5ae1a
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-06-26 20:06:27 +0000

    added skip-name-resolve=0

--- rootfs/usr/local/bin/replication_init.sh
+++ rootfs/usr/local/bin/replication_init.sh
@@ -51,6 +51,7 @@ function replication_init_sql(){
 function replication_init_cnf(){
     REPLICATION_CNF="$(replication_cnf)"
     echo "[mariadb]" >> "$REPLICATION_CNF"
+    echo "skip-name-resolve=0" >> "$REPLICATION_CNF"
     echo "log-bin=mysql-bin" >> "$REPLICATION_CNF"
     echo "binlog-do-db=${APP_NAME}" >> "$REPLICATION_CNF"
     echo "relay-log=mysql-relay-bin" >> "$REPLICATION_CNF"
