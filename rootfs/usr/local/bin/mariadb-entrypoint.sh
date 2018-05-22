784920ed5adbc9674b29b89599d07490bc6237a9
Greg Junge <gregnuj@gmail.com>
2018-05-21 22:18:28 -0500

    Update mariadb-entrypoint.sh

--- rootfs/usr/local/bin/mariadb-entrypoint.sh
+++ rootfs/usr/local/bin/mariadb-entrypoint.sh
@@ -8,6 +8,14 @@ if [[ -n "$DEBUG" ]]; then
     set -x
 fi
 
+# copy /configs files
+configs="$(find /configs/ -type f -exec ls  {} \; 2> /dev/null | awk  '!/\/\./{print substr($0,9)}')"
+if [ -n "$configs" ]; then
+    for config in $configs; do
+        cp --preserve=all "/configs/${config}" "${config}"
+    done;
+fi
+
 # capture parameters
 declare -a cmd=( "$@" )
 
